219dc2cd48ec35f24ef09eb73ba940c039ca145c
mushtariy321 <mushtariyrixsiboyev5a@gmail.com>
2024-04-17 15:19:00 +0300

    done

---
+++ main.js
@@ -0,0 +1,13 @@
+// let n1 = prompt('ismingizni kiriting!')
+// let n2 = prompt('ism kiriting!')
+// console.log(n1 === n2);
+
+
+
+let string = prompt('String type dagi malumotni kiriting')
+let number = +prompt('Number type dagi malumotni kiriting')
+let boolean = prompt('Boolean type dagi malumotni kiriting')
+let con = confirm('Malumotlarni togri kiritganingizga ishonchingiz komilmi?')
+
+console.log(`siz kiritgan ${string} string typega tegishli \n siz kiritgan ${number} numberr typega tegishli \nsiz kiritgan ${boolean} boolean typega tegishli`);
+
